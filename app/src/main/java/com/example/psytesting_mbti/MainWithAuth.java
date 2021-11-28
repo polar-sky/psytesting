b56e27fa6c8d7d7c88fc22660f0398c044a7cf4b
Inu07040 <inu07040@mail.ru>
2021-11-28 23:33:03 +0300

    небольшие изменения макетов

---
+++ app/src/main/java/com/example/psytesting_mbti/MainWithAuth.java
@@ -0,0 +1,14 @@
+package com.example.psytesting_mbti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainWithAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_with_auth);
+    }
+}
